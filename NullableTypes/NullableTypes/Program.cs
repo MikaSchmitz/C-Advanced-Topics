1bdede108c8f210735cb9a475ff167ff78fc4173
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2022-05-07 18:39:21 +0200

    Nullable types

---
+++ NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿// valuetypes cannot be null (for example DateTime)
+// nullable types can be done using Nullable<T> or by using a '?'
+
+DateTime? date = null;
+
+//nullable has preset methods
+Console.WriteLine("GetValueOrDefault(): " + date.GetValueOrDefault());
+Console.WriteLine("HasValue: " + date.HasValue);
+
+//nulable to non-nullable can be done using GetValueOrDefault()
+DateTime? date2 = DateTime.Now;
+DateTime date3 = date2.GetValueOrDefault();
+
+//with tertiary operator
+DateTime date4 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+
+Console.WriteLine(date4);
